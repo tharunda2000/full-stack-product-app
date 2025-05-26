e4865550098c55947315160e9db75511efea43ed
Tharunda De Silva <tharundadesilva@gmail.com>
2025-05-26 14:11:56 +0530

    Backend - database connected

---
+++ Backend/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+
+const connectDB = async () => {
+    try{
+
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`MongoDB connected : ${conn.connection.host}`)
+
+    }catch(error){
+
+        console.log(error)
+
+    }
+
+}
+
+export default connectDB;
--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,18 @@
-const express =  require('express')
+import express from 'express'
+import dotEnv from 'dotenv'
+import connectDB from './config/db.js'
 
+
+dotEnv.config()
 const app = express()
 
+
 app.listen(5000,()=>{
+    connectDB()
     console.log('server running on port 5000...')
 })
 
 app.get('/',(req,res)=>{
     res.send('server is reddy')
 })
+
