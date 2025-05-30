a4de2b7f2a64a6dd324a6a67ea3e971e05851d8c
Tharunda De Silva <tharundadesilva@gmail.com>
2025-05-30 19:48:47 +0530

    Backend - put method added

--- Backend/Routes/productRoute.js
+++ Backend/Routes/productRoute.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import Product from '../models/productModel.js'
 const router = express.Router();
+import mongoose from 'mongoose'
 
 //post method
 
@@ -84,6 +85,28 @@ router.get('/', async (req,res)=>{
     }
 
 
+})
+
+//Update a product method
+
+router.put('/:id', async (req,res)=> {
+    const id = req.params.id;
+    const product = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success:false,message:'invalid product id'})
+    }
+
+    try{
+
+        const newProduct = await Product.findByIdAndUpdate(id,product,{new:true});
+        res.status(200).json({success:true,data:newProduct});
+
+    }catch(error){
+
+        res.status(500).json({success:false,message:"Server error"});
+
+    }
 })
 
 
